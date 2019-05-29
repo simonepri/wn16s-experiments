2a8484452a9cdb4ddf7fcf2da4fee804e610159f
simonepri <simonepri@outlook.com>
2019-05-29 19:31:46 +0200

    .

---
+++ configs/wn16s_config.py
@@ -0,0 +1,41 @@
+def get_torchbiggraph_config():
+  config = dict(
+    # I/O data
+    entity_path = 'data/wn16s',
+    edge_paths = [],
+    checkpoint_path = 'model/wn16s',
+    init_path = null,
+
+    # Graph structure
+    entities = {
+      'all': {'num_partitions': 1},
+    },
+    relations = [{
+      'name': 'all_edges',
+      'lhs': 'all',
+      'rhs': 'all',
+      'operator': 'translation',
+    }],
+    dynamic_relations = True,
+
+    # Scoring model
+    dimension = 128,
+    max_norm = null,
+    global_emb = False,
+    comparator = 'dot',
+    bias = False,
+
+    # Training
+    num_epochs = 50,
+    batch_size = 1000,
+    num_batch_negs = 100,
+    num_uniform_negs = 0,
+    loss_fn = 'softmax',
+    margin = 0.1,
+    lr = 0.1,
+
+    # Evaluation during training
+    eval_fraction = 0,
+  )
+
+  return config
---
+++ wn16s.py
@@ -0,0 +1,72 @@
+import argparse
+import os
+from itertools import chain
+
+import attr
+import pkg_resources
+
+import torchbiggraph.converters.utils as utils
+from torchbiggraph.config import parse_config
+from torchbiggraph.converters.import_from_tsv import convert_input_data
+from torchbiggraph.eval import do_eval
+from torchbiggraph.train import train
+
+DATA_DIR = 'data'
+WN16S_URL = 'https://github.com/simonepri/WN16S/releases/latest/download/WN16S.tgz'
+FILENAMES = {
+  'train': 'edge2id_all.tsv',
+  'test': 'edge2id_all.tsv',
+}
+DEFAULT_CONFIG = pkg_resources.resource_filename('configs/wn16s_config.py')
+
+def convert_path(fname):
+  basename, _ = os.path.splitext(fname)
+  out_dir = basename + '_partitioned'
+  return out_dir
+
+
+def main():
+  parser = argparse.ArgumentParser(description='Example on FB15k')
+  parser.add_argument('--config', default = DEFAULT_CONFIG,
+                      help='Path to config file')
+  parser.add_argument('-p', '--param', action='append', nargs='*')
+  parser.add_argument('--data_dir', default='data',
+                      help='where to save processed data')
+  parser.add_argument('--no-filtered', dest='filtered', action='store_false',
+                      help='Run unfiltered eval')
+  args = parser.parse_args()
+
+  if args.param is not None:
+      overrides = chain.from_iterable(args.param)  # flatten
+  else:
+      overrides = None
+
+  data_dir = args.data_dir
+  fpath = utils.download_url(WN16S_URL, DATA_DIR)
+  utils.extract_tar(fpath)
+
+  edge_paths = [os.path.join(DATA_DIR, name) for name in FILENAMES.values()]
+  convert_input_data(
+    args.config,
+    edge_paths,
+    lhs_col = 0,
+    rhs_col = 2,
+    rel_col = 1,
+  )
+
+  config = parse_config(args.config, overrides)
+
+  train_path = [convert_path(os.path.join(DATA_DIR, FILENAMES['train']))]
+  train_config = attr.evolve(config, edge_paths = train_path)
+
+  train(train_config)
+
+  eval_path = [convert_path(os.path.join(DATA_DIR, FILENAMES['test']))]
+  relations = [attr.evolve(r, all_negs = True) for r in config.relations]
+  eval_config = attr.evolve(config, edge_paths = eval_path, relations = relations)
+
+  do_eval(eval_config)
+
+
+if __name__ == '__main__':
+  main()
