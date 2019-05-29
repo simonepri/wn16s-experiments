dd4c577b968da2a89831081f9f4e383f7bbe0533
simonepri <simonepri@outlook.com>
2019-05-29 23:11:29 +0200

    .

--- configs/wn16s_config.py
+++ configs/wn16s_config.py
@@ -14,7 +14,7 @@ def get_torchbiggraph_config():
       'name': 'all_edges',
       'lhs': 'all',
       'rhs': 'all',
-      'operator': 'translation',
+      'operator': 'affine',
     }],
     dynamic_relations = True,
 
