b58a297918451120d550eedb1569597fadc30ca0
simonepri <simonepri@outlook.com>
2019-06-01 10:35:03 +0200

    Revert "."
    This reverts commit a3fe253443c71db62537c7e2b5b1ea9135e5f4a1.

--- configs/wn16s_config.py
+++ configs/wn16s_config.py
@@ -14,7 +14,7 @@ def get_torchbiggraph_config():
       'name': 'all_edges',
       'lhs': 'all',
       'rhs': 'all',
-      #'pre_operator': 'linear',
+      'pre_operator': 'linear',
       'operator': 'translation',
     }],
     dynamic_relations = True,
